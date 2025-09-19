47bbd99af56fa6af74b364c26ac428ca03fb4db1
XaQorix <chababadam7@gmail.com>
2025-09-19 15:49:16 +0200

    added local storage

--- Js.js
+++ Js.js
@@ -1,9 +1,22 @@
+let data = JSON.parse(localStorage.getItem("score_hk"))
+if(!data) {
+    data = {
+        score: 0
+    }
+}
+
 const div = document.getElementById("nombre")
-let clickamount = 0
+let clickamount = data.score;
+
+function save() {
+    const json = {
+        score: clickamount
+    }
+    localStorage.setItem("score_hk", JSON.stringify(json))
+}
 
 function bonjour() {
     clickamount++
     div.textContent = clickamount
-    
-
+    save()
 }
